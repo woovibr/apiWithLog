bf8239189d03b241bd05aa303e2e2f5e29ac08c7
João Victor <68869379+JoaoVictor6@users.noreply.github.com>
2023-04-17 19:52:20 -0300

    fix: solve timeSpan implementation

--- src/timeSpan.ts
+++ src/timeSpan.ts
@@ -1,11 +1,16 @@
-import convertHrtime from 'convert-hrtime';
+import convertHrtime, { HighResolutionTime } from 'convert-hrtime';
 
 export const timeSpan = () => {
-  const start = process.hrtime();
-  const end = (type) => convertHrtime(process.hrtime(start))[type];
+  const start = process.hrtime.bigint();
+  const end = (timeMeasure: keyof HighResolutionTime) => {
+    const diff = process.hrtime.bigint() - start;
+    return Number(convertHrtime(
+      diff
+    )[timeMeasure])
+  };
 
   const returnValue = () => end('milliseconds');
-  returnValue.rounded = () => Math.round(end('milliseconds'));
+  returnValue.rounded = () => Math.round(end('nanoseconds'));
   returnValue.seconds = () => end('seconds');
   returnValue.nanoseconds = () => end('nanoseconds');
 
